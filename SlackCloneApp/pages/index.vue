a24c1d1dddfe9949cc0554e8eb79907dae749cff
kujira.free <yo.kinmu@gmail.com>
2020-03-21 17:24:17 +0900

    1.  Vueのコンポーネントを理解する

---
+++ SlackCloneApp/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="container">
+   <div class="chats-layout">
+     <messages />
+   </div>
+   <div class="input-layout">
+     <chat-form />
+   </div>
+ </div>
+</template>
+
+<script>
+import Messages from '~/components/Messages.vue'
+import ChatForm from '~/components/ChatForm.vue'
+
+export default {
+ components: {
+   Messages,
+   ChatForm
+ }
+}
+</script>
